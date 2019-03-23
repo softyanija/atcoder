72d3949fed29dd54e9514adf42afd21a6bea9d12
akaino <amakou0614@gmail.com>
2019-03-24 00:46:16 +0900

    088B実装した

--- ABC088B.cpp
+++ ABC088B.cpp
@@ -1,12 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b)
+{
+    return *(int *)b - *(int *)a;
+}
 
 int main(){
-    int n;
-    scanf("%d",n);
-    int s[n];
-    for(int i=0;i<n;i++){
-        scan("%d",&s[i]);
+    int i,n,sum=0;
+        int a[101]={0};
+    scanf("%d\n", &n);//”z—ñ‚Ì—v‘f”
+
+    for(i=0;i<n;i++){
+        scanf("%d", &a[i]);
     }
 
+    qsort(a,n,4,compare);
 
+    for(i=0;i<n;i++){
+        if(i % 2 == 0){
+            sum += a[i];
+        }
+        else{
+            sum -= a[i];
+        }
+    }
+    printf("%d",sum);
 }
